5f0182a89f88272541564b894db83cc08350a987
sofiapetushok <sofiapetushok@outlook.com>
2023-12-12 22:39:30 +0200

    paths

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ console.log(23 + 97);
 let a = 10;
 9 * a;
 let b = 7 * a;
-console.log(b);
-console.log(a);
-// ------------------
+// console.log(b);
+// console.log(a);
+// // ------------------
 
-let max = 57;
-let actual = max - 13;
-let percentage = actual/max;
-console.log(percentage);
+// let max = 57;
+// let actual = max - 13;
+// let percentage = actual/max;
+// console.log(percentage);
