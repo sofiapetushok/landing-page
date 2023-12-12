3bad9fe08cd68b625212a146ab78ad4cfcfbd729
sofiapetushok <sofiapetushok@outlook.com>
2023-12-12 22:20:37 +0200

    last changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+console.log("hello");
+ 
+console.log(23 + 97);
+let a = 10;
+9 * a;
+let b = 7 * a;
+console.log(b);
+console.log(a);
+// ------------------
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+console.log(percentage);
